0a92c8c911c9ce3dcab4c0b2e03471bd6fb9c0ee
Elrey Belmonti <elbelmonti@gmail.com>
2017-09-29 13:32:14 -0700

    create route to add brands to database and return to previous page

--- app.rb
+++ app.rb
@@ -10,30 +10,69 @@ get("/") do
   erb(:index)
 end
 
-get("/store_list") do
+get("/stores_list") do
   @stores = Store.all
-  erb(:store_list)
+  erb(:stores_list)
 end
 
-get("/stores/:id") do
+get("/brands_list") do
+  @brands = Brand.all
+  erb(:brands_list)
+end
+
+get("/store/:id") do
   @store = Store.find(params[:id])
-  erb(:stores)
+  erb(:store)
 end
 
 post("/add_store") do
   store = Store.new({name: params['name']})
-  store.save
-  redirect 'stores_list'
+  if store.save
+    redirect 'stores_list'
+  else
+    @error_type = store
+    @route = "/stores_list"
+    erb(:errors)
+  end
 end
+
+post("/add_brand") do
+  new_brand = Brand.new({name: params['name'], price: params['price']})
+
+  if new_brand.save
+    redirect 'brands_list'
+  else
+    @error_type = new_brand
+    @route = "/brands_list"
+    erb(:errors)
+  end
+end
+
+# post("/add_brand/:id") do
+#   store = Store.find(params[:id])
+#   brand_name = params['name']
+#   brand_price = params['price']
+#   brand = Brand.find_or_initialize(name: brand_name, price: brand_price)
+#
+#   if new_brand.id
+#     if !store.brands.include?(new_brand)
+#       store.brands.push(new_brand)
+#     end
+#   else
+#     new_brand.save
+#     store.brands.push(new_brand)
+#   end
+#   redirect back
+# end
 
 patch("/update_name/store/:id") do
   store = Store.find(params[:id])
   store.update({name: params['update_name']})
-  erb redirect "stores/#{store.id}"
+  erb redirect "store/#{store.id}"
 end
 
 delete("/delete_store/:id") do
   store = Store.find(params[:id])
   Store.where(id: store.id).destroy_all
-  redirect "store_list"
+  redirect "stores_list"
 end
