d54a71773d762a88c3581f673b140e96ef510340
Elrey Belmonti <elbelmonti@gmail.com>
2017-09-29 09:27:01 -0700

    create #capitalizes_name for Store

--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,12 @@
 class Store < ActiveRecord::Base
   has_many :brands, through: :brands_stores
+
+  before_save(:capitalizes_name)
+
+  private
+
+  def capitalizes_name
+    name = self.name.split(" ")
+    self.name = name.map{ |word| word.capitalize }.join(" ")
+  end
 end
--- spec/store_spec.rb
+++ spec/store_spec.rb
@@ -1,1 +1,12 @@
 require 'spec_helper'
+
+describe 'Store' do
+  let(:store) { Store.new({name: "nike store"})}
+
+  describe '#capitalizes_name' do
+    it 'will capitalizes every word in store name' do
+      store.save
+      expect(store.name).to eq "Nike Store"
+    end
+  end
+end
